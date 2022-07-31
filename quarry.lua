ff7723313b651f39130d3eb488a706878123ffaa
Aldo Solís <68377037+aldosolisr@users.noreply.github.com>
2022-07-31 01:57:14 -0500

    quarry2.0
    Arreglo de errores y nuevas funcionalidades

--- quarry.lua
+++ quarry.lua
@@ -1,29 +1,40 @@
 local turnTo = 1 --flag para indicar a donde dara la vuelta
 local i = 1
 local j = 1
+local times = 1
 while true do
 for i = 1, 16, 1 do
 
     for j = 1, 15, 1 do
         turtle.digDown()
-        turtle.forward()
+        if turtle.forward() == false then
+            turtle.dig()
+            turtle.forward()
+        end
     end
-
-    if turnTo > 0 then
+    if times == 16 then
         turtle.turnLeft()
         turtle.digDown()
-        turtle.forward()
+        turtle.down()
+        times = 0
+    elseif turnTo > 0 then
         turtle.turnLeft()
-        turnTo = turnTo * -1
-    
+        turtle.digDown()
+        if turtle.forward() == false then
+            turtle.dig()
+            turtle.forward()
+        end
+        turtle.turnLeft()
     elseif turnTo < 0 then 
         turtle.turnRight()
         turtle.digDown()
-        turtle.forward()
+        if turtle.forward() == false then
+            turtle.dig()
+            turtle.forward()
+        end
         turtle.turnRight()
-        turnTo = turnTo * -1
     end
+    times = times + 1
+    turnTo = turnTo * -1
 end
-    turtle.down()
-      turtle.turnRight()
 end
